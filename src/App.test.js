3f73d80f7bf2a4a94814d6cb9b53a9fec99b7df6
mattia richetto <mattia.richetto@gmail.com>
2022-01-17 19:24:36 -0500

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const heading = screen.getByText('TODO');
+  const heading = screen.getByText("Bell's palsy - Mattia's journey");
   expect(heading).toBeInTheDocument();
 });
