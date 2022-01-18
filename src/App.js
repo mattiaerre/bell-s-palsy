4505c4290d7aabd81f1e4aef1e9b4e07eeeb0401
mattia richetto <mattia.richetto@gmail.com>
2022-01-17 19:00:38 -0500

    scaffolding and the main app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import paths from './files.json';
 import './App.css';
 
 function App() {
+  const [imgSrc, setImgSrc] = useState(paths[paths.length - 1]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bell's palsy - Mattia's journey</h1>
+      <img alt="TODO" src={imgSrc} />
+      <input
+        label={imgSrc.split('/')[1]}
+        list="tickmarks"
+        max={paths.length - 1}
+        min="0"
+        onChange={(e) => setImgSrc(paths[parseInt(e.target.value)])}
+        type="range"
+      />
+      <datalist id="tickmarks">
+        {paths.map((_, index) => (
+          <option key={paths[index]} value={index}></option>
+        ))}
+      </datalist>
+      <footer>bell-s-palsy v0.1.0 - 2022</footer>
     </div>
   );
 }
