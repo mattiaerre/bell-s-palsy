4b96466d19a07ffc93ddd67c7e7083e94b49cde2
mattia richetto <mattia.richetto@gmail.com>
2022-01-17 20:12:29 -0500

    add notes

--- src/App.js
+++ src/App.js
@@ -26,6 +26,13 @@ function App() {
         ))}
       </datalist>
       <p>Last update: {getLastUpdate(paths)}</p>
+      <h2>Notes</h2>
+      <ul>
+        <li>My journey started Sunday, January 16th, 2022</li>
+        <li>I'm taking a pic a day around 2:00 pm EST</li>
+        <li>The picture shows my biggest smile</li>
+        <li>You can slide left to go back in time</li>
+      </ul>
       <footer>bell-s-palsy v0.1.2 - 2022</footer>
     </div>
   );
