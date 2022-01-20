6d2defec0e6e475382b7d739903020abe8f747de
Mattia Richetto <mattia.richetto@gmail.com>
2022-01-19 20:25:19 -0500

    2022-01-19 (#5)

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
           </a>
         </li>
       </ul>
-      <footer>bell-s-palsy v0.3.0 - 2022</footer>
+      <footer>bell-s-palsy v0.3.1 - 2022</footer>
     </div>
   );
 }
