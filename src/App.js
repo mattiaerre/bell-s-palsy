c4c13ca7bccbfa95a9e694f11850e0f2eb04c496
mattia richetto <mattia.richetto@gmail.com>
2022-01-21 16:53:32 -0500

    bump version

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
           </a>
         </li>
       </ul>
-      <footer>bell-s-palsy v0.4.0 - 2022</footer>
+      <footer>bell-s-palsy v0.4.1 - 2022</footer>
     </div>
   );
 }
