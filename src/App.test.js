9db0ce40e772b1277f3df024303c0af9473dfb82
mattia richetto <mattia.richetto@gmail.com>
2022-01-22 17:33:35 -0500

    add test wip

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('snapshot', () => {
+  const { asFragment } = render(<App />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test("it renders Bell's palsy - Mattia's journey", () => {
   render(<App />);
   const heading = screen.getByText("Bell's palsy - Mattia's journey");
   expect(heading).toBeInTheDocument();
+});
+
+test.skip('scroll', () => {
+  render(<App />);
+  const slider = screen.getByRole('slider');
+  expect(slider).toMatchSnapshot();
+  fireEvent.change(slider, { taget: { value: 0 } });
+  expect(screen.getByText('1/6')).toBeInTheDocument();
 });
