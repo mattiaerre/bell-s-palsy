ef95e35836b43e5baff8fd911ddccbfd4d302518
mattia richetto <mattia.richetto@gmail.com>
2022-01-23 14:28:41 -0500

    all the things

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,29 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
+const mockPaths = [
+  'images/2022_01_20.jpg',
+  'images/2022_01_21.jpg',
+  'images/2022_01_22.jpg'
+];
+
+const mockVersion = '0.5.0';
+
 test('snapshot', () => {
-  const { asFragment } = render(<App />);
+  const { asFragment } = render(
+    <App paths={mockPaths} version={mockVersion} />
+  );
   expect(asFragment()).toMatchSnapshot();
 });
 
 test("it renders Bell's palsy - Mattia's journey", () => {
-  render(<App />);
+  render(<App paths={mockPaths} version={mockVersion} />);
   const heading = screen.getByText("Bell's palsy - Mattia's journey");
   expect(heading).toBeInTheDocument();
 });
 
 test.skip('scroll', () => {
-  render(<App />);
+  render(<App paths={mockPaths} version={mockVersion} />);
   const slider = screen.getByRole('slider');
   expect(slider).toMatchSnapshot();
   fireEvent.change(slider, { taget: { value: 0 } });
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import timestamp from 'jest-mock-now';
+
+timestamp(new Date(1642960800000)); // Sunday, January 23, 2022 1:00:00 PM GMT-05:00
