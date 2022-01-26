d89aa3450bd998468045c51e73ece13830729750
Mattia Richetto <mattia.richetto@gmail.com>
2022-01-25 20:27:58 -0500

    new pic (#11)

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App paths={paths} version="0.5.1" />
+    <App paths={paths} version="0.5.2" />
   </React.StrictMode>,
   document.getElementById('root')
 );
