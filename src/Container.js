bdcb15d8fc2d9914583162de38f7e4cbb9777e0e
mattia richetto <mattia.richetto@gmail.com>
2022-01-27 13:57:38 -0500

    bump version

--- src/Container.js
+++ src/Container.js
@@ -12,7 +12,7 @@ function Container() {
   if (authorization !== 'authorized') {
     return <Playground callback={setAuthorization} />;
   }
-  return <App callback={setAuthorization} paths={paths} version="0.7.8" />;
+  return <App callback={setAuthorization} paths={paths} version="0.8.0" />;
 }
 
 export default Container;
