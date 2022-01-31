60f44287bf718ea0198b186cc042a5cf3692038e
mattia richetto <mattia.richetto@gmail.com>
2022-01-31 17:42:08 -0500

    code cov

--- src/Playground.test.js
+++ src/Playground.test.js
@@ -1,11 +1,20 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Playground from './Playground';
 
 test('snapshot', () => {
-  const { asFragment } = render(<Playground callback={jest.fn()} />);
+  const { asFragment } = render(
+    <Playground callback={jest.fn()} password="TODO" />
+  );
   expect(asFragment()).toMatchSnapshot();
 });
 
 test('authorized', () => {
-  // TODO
+  const mockCallback = jest.fn();
+  render(<Playground callback={mockCallback} password="YOLO" />);
+  const inputs = screen.getAllByRole('textbox');
+  fireEvent.change(inputs[0], { target: { value: 'Y' } });
+  fireEvent.change(inputs[1], { target: { value: 'O' } });
+  fireEvent.change(inputs[2], { target: { value: 'L' } });
+  fireEvent.change(inputs[3], { target: { value: 'O' } });
+  expect(mockCallback).toBeCalledWith('authorized');
 });
