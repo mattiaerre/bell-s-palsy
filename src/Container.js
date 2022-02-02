8cebd8408942e398602d7556fa49cb67bb6043ad
mattia richetto <mattia.richetto@gmail.com>
2022-02-01 20:15:13 -0500

    bump version

--- src/Container.js
+++ src/Container.js
@@ -15,7 +15,7 @@ function Container() {
       password={process.env.REACT_APP_PASSWORD}
       paths={paths}
       sessions={sessions}
-      version="0.9.9"
+      version="0.10.0"
     />
   );
 }
