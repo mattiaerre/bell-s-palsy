913913fc86996ff1a9d79354bddd6dce2dec27ce
Mattia Richetto <mattia.richetto@gmail.com>
2022-02-10 13:51:39 -0500

    new pic and stuff (#30)

--- src/Container.js
+++ src/Container.js
@@ -3,7 +3,7 @@ import sessions from './acupuncture.json';
 import App from './App';
 import paths from './files.json';
 
-const version = '0.13.3';
+const version = '0.13.4';
 
 function Container() {
   const [authorization, setAuthorization] = useLocalStorage(
